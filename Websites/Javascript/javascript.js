a90325e6a633d7deb2a38bb5680a4d22b7e80716
Tucker5623 <tajohnson7@uh.edu>
2020-10-14 07:44:29 -0500

    Add files via upload

---
+++ Websites/Javascript/javascript.js
@@ -0,0 +1,69 @@
+//Variable only
+$("#varibut").click(function () {
+   $(".noundist").show();
+   $(".nodept").show();
+   $(".control").show();
+   $(".uncontrol").show();
+   $(".fixed").show();
+   $(".vari").show();
+   $(".fixed").fadeToggle();
+});
+//Fixed Only
+$("#varifix").click(function () {
+   $(".noundist").show();
+   $(".nodept").show();
+   $(".control").show();
+   $(".uncontrol").show();
+   $(".fixed").show();
+   $(".vari").show();
+   $(".vari").fadeToggle();
+});
+//Controllable only
+$("#varicont").click(function () {
+   $(".noundist").show();
+   $(".nodept").show();
+   $(".control").show();
+   $(".uncontrol").show();
+   $(".fixed").show();
+   $(".vari").show();
+   $(".uncontrol").fadeToggle();
+});
+//Uncontrollable only
+$("#variuncont").click(function () {
+   $(".noundist").show();
+   $(".nodept").show();
+   $(".control").show();
+   $(".uncontrol").show();
+   $(".fixed").show();
+   $(".vari").show();
+   $(".control").fadeToggle();
+});
+//Departmental only
+$("#varidept").click(function () {
+   $(".noundist").show();
+   $(".nodept").show();
+   $(".control").show();
+   $(".uncontrol").show();
+   $(".fixed").show();
+   $(".vari").show();
+   $(".nodept").fadeToggle();
+});
+//Undistributed only
+$("#variundist").click(function () {
+   $(".noundist").show();
+   $(".nodept").show();
+   $(".control").show();
+   $(".uncontrol").show();
+   $(".fixed").show();
+   $(".vari").show();
+   $(".noundist").fadeToggle();
+});
+//Show All
+$("#varires").click(function () {
+   $(".noundist").show();
+   $(".nodept").show();
+   $(".control").show();
+   $(".uncontrol").show();
+   $(".fixed").show();
+   $(".vari").show();
+});
